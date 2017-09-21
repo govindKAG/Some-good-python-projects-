0fea4fdf6ba9221178dde40da75435d02a2323df
govind cs <govindsomalal7@gmail.com>
2017-09-21 21:32:45 +0530

    fact

--- oneliners.py
+++ oneliners.py
@@ -2,3 +2,10 @@ def primeoneliner(lim):
         """ lim : int , upper bound non inclusive
             this function implements the sieve of erastothens method in oneline"""
         return [x for x in range(2,lim) if x not in [j for i in (2,3,5,7) for j in range (i*2,lim,i)]]
+
+def fact(num):
+	if num <=1 :
+		return 1
+
+	return reduce(lambda a,b : a*b,range(1,num+1))
+
