dc01944d7d18f6683033700d40bcb9906cd6a64a
govind cs <govindsomalal7@gmail.com>
2017-09-23 15:54:10 +0530

    fixed

--- substringcounter.py
+++ substringcounter.py
@@ -1,10 +1,11 @@
- def substringcounter(substrings, string,caseSensitive = False):
+def substringcounter(substrings, string,caseSensitive = False):
 
 	if  caseSensitive == False:
 		string = string.lower()
 		substrings = set(map(str.lower,substrings))
 	counts = dict.fromkeys(substrings,0)
 	for i in substrings:
-		if i in string:
-			counts[i] += 1
+		for j in string.split(" "):
+			if i in j:
+				counts[i] += 1
 	return counts
