7a179e1eabcf15b0a6503af4dbb83b687cc0961c
govind cs <govindsomalal7@gmail.com>
2019-01-02 17:37:51 +0530

    Delete emailTest.py

--- emailTest.py
+++
@@ -1,9 +0,0 @@
-import smtplib
- 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login("kagarmy@gmail.com", "iamkaghellyeah")
- 
-msg = "this is all teh message there is today i am afraid"
-server.sendmail("kagarmy@gmail.com", "govindsomalal7@gmail.com", msg)
-server.quit()
